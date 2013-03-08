c8d9f7857538e52ff0e6d51c6cc7d429a8fc3ec8
Michal Frystacky <MFrystacky2009@my.fit.edu>
2013-03-08 02:15:04 -0500

    Merging code

---
+++ src/Tree.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: michalfrystacky
+ * Date: 3/2/13
+ * Time: 7:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Tree {
+}
